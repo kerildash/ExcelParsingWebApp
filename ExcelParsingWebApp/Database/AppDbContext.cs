48c715463ed59ca2569fd0bc32391d542c7a4a5a
kerildash <kerildash@gmail.com>
2024-08-28 19:26:59 +0300

    Remove unnesesary usings.

--- ExcelParsingWebApp/Database/AppDbContext.cs
+++ ExcelParsingWebApp/Database/AppDbContext.cs
@@ -1,11 +1,10 @@
 ﻿using ExcelParsingWebApp.Models.Database;
-using ExcelParsingWebApp.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExcelParsingWebApp.Database;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public DbSet<AccountDto> Accounts { get; set; }
-    public DbSet<ClassDto> Classes { get; set; }
-    public DbSet<SheetDto> Sheets { get; set; }
+	public DbSet<AccountDto> Accounts { get; set; }
+	public DbSet<ClassDto> Classes { get; set; }
+	public DbSet<SheetDto> Sheets { get; set; }
 }
