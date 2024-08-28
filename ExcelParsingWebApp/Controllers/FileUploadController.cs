bc72c75d542886e7c703b5a46bf3e3285dbcff97
kerildash <kerildash@gmail.com>
2024-08-28 19:41:17 +0300

    Change Upload() action to view the uploaded sheet if the uploading was successful

--- ExcelParsingWebApp/Controllers/FileUploadController.cs
+++ ExcelParsingWebApp/Controllers/FileUploadController.cs
@@ -6,100 +6,99 @@ using ExcelParsingWebApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using ExcelParsingWebApp.Models;
 
-namespace ExcelParsingWebApp.Controllers
+namespace ExcelParsingWebApp.Controllers;
+
+public class FileUploadController(
+IFileService fileService,
+IWorksheetReader worksheetReader,
+SheetRepository sheetRepo,
+ClassRepository classRepo,
+AccountRepository accountRepo
+) : Controller
 {
-    public class FileUploadController(
-		IFileService fileService,
-		IWorksheetReader worksheetReader,
-		SheetRepository sheetRepo,
-		ClassRepository classRepo,
-		AccountRepository accountRepo
-		) : Controller
+	[HttpGet]
+	public IActionResult Index()
 	{
-		[HttpGet]
-		public IActionResult Index()
-		{
-			return Upload();
-		}
-		[HttpGet]
-		public ActionResult Upload()
-		{
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> Upload(UploadViewModel model)
-		{
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-            if (model.File != null && model.File.Length > 0)
-            {
-				string filePath = await fileService.CreateAsync(model);
-
-                await worksheetReader.CreateAsync(filePath);
-				Sheet sheet = await worksheetReader.ReadHeaderAsync();
-
-				await sheetRepo.CreateAsync(
-					new SheetDto
-					{
-						Id = sheet.Id,
-						SheetTitle = sheet.SheetTitle,
-						BankName = sheet.BankName,
-						PeriodInfo = sheet.PeriodInfo,
-						AdditionalInfo = sheet.AdditionalInfo,
-						Date = sheet.Date,
-						Currency = sheet.Currency
-					});
-
-				Class? c = null;
-				Account? account;
-				while (worksheetReader.GoNextRaw())
-				{
-					ContentType type = worksheetReader.CheckContentType();
-					switch (type)
-					{
-						case ContentType.Empty:
-							break;
-						case ContentType.Class:
-                            c = worksheetReader.ReadClass();
-
-                            await classRepo.CreateAsync(
-                                new ClassDto
-                                {
-                                    Id = c.Id,
-                                    SheetId = sheet.Id,
-                                    ClassName = c.ClassName,
-                                });
-							break;
-						case ContentType.Account:
-                            account = worksheetReader.ReadAccount();
-                            if (account != null)
-                            {
-                                await accountRepo.CreateAsync(
-                                    new AccountDto
-                                    {
-                                        Id = account.Id,
-                                        ClassId = c.Id,
-                                        IncomingBalanceAssets = account.IncomingBalanceAssets,
-                                        IncomingBalanceLiabilities = account.IncomingBalanceLiabilities,
-                                        Debit = account.Debit,
-                                        Credit = account.Credit
-                                    });
-                            }
-							break;
-						default:
-							break;
-                    }
-
-				}
-                worksheetReader.Close();
-                return Ok($"File uploaded successfully: {model.File.FileName}, {await worksheetReader.GetWorksheetNameAsync()}");
-				
-			}
-
-            return BadRequest("No file selected.");
-        }
-		
+		return Upload();
 	}
+	[HttpGet]
+	public ActionResult Upload()
+	{
+		return View();
+	}
+	[HttpPost]
+	public async Task<IActionResult> Upload(UploadViewModel model)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(model);
+		}
+		if (model.File != null && model.File.Length > 0)
+		{
+			string filePath = await fileService.CreateAsync(model);
+
+			await worksheetReader.CreateAsync(filePath);
+			Sheet sheet = await worksheetReader.ReadHeaderAsync();
+
+			await sheetRepo.CreateAsync(
+				new SheetDto
+				{
+					Id = sheet.Id,
+					SheetTitle = sheet.SheetTitle,
+					BankName = sheet.BankName,
+					PeriodInfo = sheet.PeriodInfo,
+					AdditionalInfo = sheet.AdditionalInfo,
+					Date = sheet.Date,
+					Currency = sheet.Currency
+				});
+
+			Class? c = null;
+			Account? account;
+			while (worksheetReader.GoNextRaw())
+			{
+				ContentType type = worksheetReader.CheckContentType();
+				switch (type)
+				{
+					case ContentType.Empty:
+						break;
+					case ContentType.Class:
+						c = worksheetReader.ReadClass();
+
+						await classRepo.CreateAsync(
+							new ClassDto
+							{
+								Id = c.Id,
+								SheetId = sheet.Id,
+								ClassName = c.ClassName,
+							});
+						break;
+					case ContentType.Account:
+						account = worksheetReader.ReadAccount();
+						if (account != null)
+						{
+							await accountRepo.CreateAsync(
+								new AccountDto
+								{
+									Id = account.Id,
+									ClassId = c.Id,
+									IncomingBalanceAssets = account.IncomingBalanceAssets,
+									IncomingBalanceLiabilities = account.IncomingBalanceLiabilities,
+									Debit = account.Debit,
+									Credit = account.Credit
+								});
+						}
+						break;
+					default:
+						break;
+				}
+
+			}
+			worksheetReader.Close();
+			return RedirectToAction("Get", "Sheet", new { id = sheet.Id });
+
+		}
+
+		return BadRequest("No file selected.");
+	}
+
 }
