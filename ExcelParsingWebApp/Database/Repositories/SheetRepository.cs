ab18cb8f19238002c4e609a77c07972907b4d8b7
kerildash <kerildash@gmail.com>
2024-08-28 19:28:19 +0300

    Remove unnessesary usings.

--- ExcelParsingWebApp/Database/Repositories/SheetRepository.cs
+++ ExcelParsingWebApp/Database/Repositories/SheetRepository.cs
@@ -1,31 +1,30 @@
 ﻿using ExcelParsingWebApp.Models.Database;
-using ExcelParsingWebApp.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExcelParsingWebApp.Database.Repositories;
 
 public class SheetRepository(AppDbContext context)
 {
-    public async Task<SheetDto> GetAsync(Guid Id)
-    {
-        if (!await context.Sheets.AnyAsync(sh => sh.Id == Id))
-        {
-            throw new ArgumentException($"Sheet with id {Id} not found");
-        }
-        return await context.Sheets.FirstAsync(sh => sh.Id == Id);
-    }
-    public async Task<List<SheetDto>> GetAllAsync()
-    {
-        
-        return await context.Sheets.ToListAsync();
-    }
-    public async Task CreateAsync(SheetDto sh)
-    {
-        await context.AddAsync(sh);
-        await SaveAsync();
-    }
-    public async Task SaveAsync()
-    {
-        await context.SaveChangesAsync();
-    }
+	public async Task<SheetDto> GetAsync(Guid Id)
+	{
+		if (!await context.Sheets.AnyAsync(sh => sh.Id == Id))
+		{
+			throw new ArgumentException($"Sheet with id {Id} not found");
+		}
+		return await context.Sheets.FirstAsync(sh => sh.Id == Id);
+	}
+	public async Task<List<SheetDto>> GetAllAsync()
+	{
+
+		return await context.Sheets.ToListAsync();
+	}
+	public async Task CreateAsync(SheetDto sh)
+	{
+		await context.AddAsync(sh);
+		await SaveAsync();
+	}
+	public async Task SaveAsync()
+	{
+		await context.SaveChangesAsync();
+	}
 }
